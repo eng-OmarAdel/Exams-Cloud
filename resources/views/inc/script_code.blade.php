b161f92825ee0c9a8bb5831b3e9d7776ccbb2db8
mmo7amed2010 <mmo7amed2010@gmail.com>
2018-10-03 21:50:20 -0700

    fixing backspace bug

--- resources/views/inc/script_code.blade.php
+++ resources/views/inc/script_code.blade.php
@@ -4,14 +4,3 @@
         
     });
 </script>
-<script type="text/javascript">
-    $('input').on('keypress', function (event) {
-    var regex = new RegExp("^[a-zA-Z0-9 ضصثقفغعهخحجدشسيبلاتنمكطئءؤرﻻىةوزظذﻵآ؟?أﻷ @._, #$&]+$");
-    var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
-    if (!regex.test(key)) {
-       event.preventDefault();
-       return false;
-    }
-});
-
-</script>
