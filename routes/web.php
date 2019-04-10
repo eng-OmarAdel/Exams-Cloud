38ec7710b4ee211a4209cbd70f42b508bbc166f3
=OmarYasserAli <3omaryasser74@gmail.com>
2019-04-10 13:35:02 +0200

    z

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,12 @@ Route::get('/logout', 'Auth\LoginController@logout');
 // Route::post('register', 'FrontController@register');
 // Route::get("emailcheck2", "FrontController@emailcheck");
 
-
+Route::get('register', 'registerController@create');
+Route::post('register', 'registerController@store');
 
 /**************************
  * admins
- * *************************/
+  * *************************/
 Route::view('/profile', 'common/profile');
 Route::get('/', 'ViewController@view');
 Route::resource('Users', 'UsersController');
@@ -19,8 +20,7 @@ Route::put('Correct/{id}', 'QuestionController@Correct');
 Route::resource('Question', 'QuestionController');
 Route::resource('Answer', 'QuestionController@answer');
 Route::get('emailcheck', 'UsersController@emailcheck');
-Route::get('register', 'registerController@create');
-Route::post('register', 'registerController@store');
+
 
 
 
