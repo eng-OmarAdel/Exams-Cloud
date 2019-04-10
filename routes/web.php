fa8058e9fe9d282f7fadf3a253295d28e16d4350
=OmarYasserAli <3omaryasser74@gmail.com>
2019-04-10 13:36:37 +0200

    9

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,6 @@ Route::get('/logout', 'Auth\LoginController@logout');
 // Route::post('register', 'FrontController@register');
 // Route::get("emailcheck2", "FrontController@emailcheck");
 
-Route::get('register', 'registerController@create');
-Route::post('register', 'registerController@store');
 
 /**************************
  * admins
