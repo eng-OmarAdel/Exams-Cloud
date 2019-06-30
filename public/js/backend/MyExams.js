de19fa7abfbd78c249b33b9098a46e324e3c671c
Abdullah-A-Elsayed <abdullah.ali26@hotmail.com>
2019-06-30 20:11:06 +0200

    preventing edit in my exams

--- public/js/backend/MyExams.js
+++ public/js/backend/MyExams.js
@@ -2,7 +2,7 @@ var tablename=document.currentScript.getAttribute("tablename");
 var cat_id=document.currentScript.getAttribute("cat_id"); //2
 var cat_type=document.currentScript.getAttribute("cat_type"); //3
 var website_url=document.currentScript.getAttribute("website_url"); //3
- //1
+var user_id=document.currentScript.getAttribute("user_id"); //
 console.log(tablename);
 var DatatablesDataSourceAjaxServer = function() {
 
@@ -69,25 +69,50 @@ var DatatablesDataSourceAjaxServer = function() {
 					render: function(data, type, full  , meta) {
 
             if (typeof full.category !== 'undefined') {
-                    showQuestions = `<a id="view" href="?view=Question&cat_id=${full.category}&cat_type=1&exam_id=${full._id}" class="dropdown-item">Manage Questions</a>`;
+                    showQuestions_ok = `<a id="view" href="?view=Question&cat_id=${full.category}&cat_type=1&exam_id=${full._id}" class="dropdown-item">Manage Questions</a>`;
             }else if (typeof full.track !== 'undefined'){
-                    showQuestions = `<a id="view" href="?view=Question&cat_id=${full.track}&cat_type=2&exam_id=${full._id}" class="dropdown-item">Manage Questions</a>`;
+                    showQuestions_ok = `<a id="view" href="?view=Question&cat_id=${full.track}&cat_type=2&exam_id=${full._id}" class="dropdown-item">Manage Questions</a>`;
 
             }
-          Solve = `<a id="view" href="?view=ExamSolve&_id=${full._id}" class="dropdown-item">Solve Exam</a>`
+          showQuestions_not_ok=``;
+          Solve_ok = `<a id="view" href="?view=ExamSolve&_id=${full._id}" class="dropdown-item">Solve Exam</a>`
+          Solve_not_ok = `<a id="view" href="#" class="dropdown-item">Can't be Solved (under construction)</a>`
+          edit_ok= `<a href="#" onclick="fill_portlet('` + full._id + `')"  class="m-portlet__nav-link btn m-btn m-btn--hover-brand m-btn--icon m-btn--icon-only m-btn--pill" title="View">
+            <i class="la la-edit"></i>
+          </a>`
+          edit_not_ok =``;
+            
+            edit =edit_not_ok;
+            showQuestions =showQuestions_not_ok;
+            if(user_id==full.ownerID ){
+                if(full.hasOwnProperty('is_editable')){
+                  if(full.is_editable == 1){
+                    edit = edit_ok;
+                    showQuestions = showQuestions_ok;
+                  }
+                }
+                else{
+                  //owner and exam not yet solved
+                  edit = edit_ok;
+                  showQuestions = showQuestions_ok;
+                }
+            }
 
-						return `
-                        <span class="dropdown">
-                            <a href="#" class="btn m-btn m-btn--hover-brand m-btn--icon m-btn--icon-only m-btn--pill" data-toggle="dropdown" aria-expanded="true">
-                              <i class="la la-ellipsis-h"></i>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right">
-                                ${showQuestions}${Solve}
-                            </div>
-                        </span>
-                        <a href="#" onclick="fill_portlet('` + full._id + `')"  class="m-portlet__nav-link btn m-btn m-btn--hover-brand m-btn--icon m-btn--icon-only m-btn--pill" title="View">
-                          <i class="la la-edit"></i>
-                        </a>`;
+            Solve = Solve_not_ok;
+            if(full.is_published == 1){
+
+              Solve = Solve_ok;
+            }
+						return  `
+            <span class="dropdown">
+                <a href="#" class="btn m-btn m-btn--hover-brand m-btn--icon m-btn--icon-only m-btn--pill" data-toggle="dropdown" aria-expanded="true">
+                  <i class="la la-ellipsis-h"></i>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right">
+                    ${showQuestions}${Solve}
+                </div>
+            </span>${edit}
+            `;
 					},
 				}
 			],
