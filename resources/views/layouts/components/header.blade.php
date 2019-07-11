9e26db41f2606222132c44e81ad572a638848622
Abdullah-A-Elsayed <abdullah.ali26@hotmail.com>
2019-07-11 03:21:35 +0200

    landing page dynamic questions categories

--- resources/views/layouts/components/header.blade.php
+++ resources/views/layouts/components/header.blade.php
@@ -9,7 +9,7 @@
 								<div class="m-stack m-stack--ver m-stack--general m-stack--inline">
 									<div class="m-stack__item m-stack__item--middle m-brand__logo">
 										<a href="{{url('')}}" class="m-brand__logo-wrapper">
-											<img alt="" src="assets/demo/media/img/logo/logo.png" />
+											<img alt="" src="assets/demo/media/img/logo/logo.png" height="80px">
 										</a>
 									</div>
 
